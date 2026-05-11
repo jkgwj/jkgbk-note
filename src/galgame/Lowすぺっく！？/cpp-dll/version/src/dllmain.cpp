7c53a98e44eeeb0f8117bd080169a8f49fe74600
jkgwj <jiekogu@gmail.com>
2026-05-11 18:42:33 +0800

    简化main

--- src/galgame/Lowすぺっく！？/cpp-dll/version/src/dllmain.cpp
+++ src/galgame/Lowすぺっく！？/cpp-dll/version/src/dllmain.cpp
@@ -5,9 +5,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
         ::DisableThreadLibraryCalls(hModule);
-        // 同步：必须在 DllMain 返回前完成导出转发
+        // 导出与转发
         real_dll_init();
-        // 异步：VirtualAlloc Hook 安装（等待 sarcheck.dll 加载）
+        //初始化
         ::CreateThread(NULL, 0, MyHook::InitHook, NULL, 0, NULL);
         break;
     case DLL_PROCESS_DETACH:
